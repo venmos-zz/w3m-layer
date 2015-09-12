d41843e565daee890dcb0657ce2a75a4ff3fd5f7
Kuroi Mato <venmos@fuck.gfw.es>
2015-09-12 23:41:30 +0800

    delete v/w3m-open-site

--- packages.el
+++ packages.el
@@ -25,10 +25,25 @@
       (evil-leader/set-key
         "awb" 'helm-w3m-bookmarks))))
 
+(defun v/w3m-open-site (site)
+  "Opens site in new w3m session with 'http://' appended"
+  (interactive
+   (list (read-string "Enter website address (default: google.com):" nil nil "google.com" nil )))
+  (w3m-goto-url
+   (concat "http://" site)))
+
+(defun v/w3m-open-site-new-session (site)
+  "Opens site in new w3m session with 'http://' appended"
+  (interactive
+   (list (read-string "Enter website address (default: google.com):" nil nil "google.com" nil )))
+  (w3m-goto-url-new-session
+   (concat "http://" site)))
+
 (defun w3m/init-w3m()
   "Initializes w3m and adds keybindings for its exposed functionalities."
   (use-package w3m
-    :commands (w3m-goto-url
+    :commands (v/w3m-open-site
+               w3m-goto-url
                w3m-goto-url-new-session
                w3m-search
                w3m-search-new-session
@@ -36,6 +51,7 @@
     :init
     (progn
       (evil-leader/set-key
+        "awo" 'v/w3m-open-site
         "awg" 'w3m-goto-url
         "awG" 'w3m-goto-url-new-session
         "aws" 'w3m-search
@@ -46,3 +62,10 @@
   (define-key w3m-mode-map (kbd "C-f") 'evil-scroll-page-down)
   (define-key w3m-mode-map (kbd "C-b") 'evil-scroll-page-up)
   (define-key w3m-mode-map (kbd "SPC") 'evil-evilified-state))
+
+(defun v/init-w3m ()
+  (use-package w3m
+    :init
+    (progn
+      (setq-default browse-url-browser-function 'w3m-goto-url-new-session)
+      (setq-default w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533."))))
